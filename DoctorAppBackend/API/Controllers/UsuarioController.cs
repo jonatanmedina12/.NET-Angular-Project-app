df0cac58508c6e43cb96325e25862326e9834577
Jonatan Medina <136943363+jonatanmedina12@users.noreply.github.com>
2024-08-19 20:38:11 -0500

    V1_0_0
    V1_0_0

---
+++ DoctorAppBackend/API/Controllers/UsuarioController.cs
@@ -0,0 +1,13 @@
+﻿using Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+    }
+}
